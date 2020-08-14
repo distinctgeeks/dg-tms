b377fba4dbddd18cc9e17ec82a354939110aa106
GoVirat <46752840+GoVirat@users.noreply.github.com>
2020-08-14 14:02:03 +0530

    adding the folder structure

---
+++ dg-tms-springboot-sql/src/main/java/com/dp/dgtmsspringbootsql/DgTmsSpringbootSqlApplication.java
@@ -0,0 +1,13 @@
+package com.dp.dgtmsspringbootsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgTmsSpringbootSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgTmsSpringbootSqlApplication.class, args);
+	}
+
+}
